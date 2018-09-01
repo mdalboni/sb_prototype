fc942b7ee5984ad35cb74dbe2ccb99c8759dd906
Maxwell Dalboni <dalboni.max@hotmail.com>
2018-09-01 17:24:32 -0300

    zoom added

--- js/zoom.js
+++ js/zoom.js
@@ -42,3 +42,60 @@ function zoomIn() {
 function getStepsDiff() {
     this.stepsDiff = (this.imageSizeRaw.width / 4);
   }
+
+  function loadHighLightImage(index) {
+    this.canvas.nativeElement.style.backgroundSize = '100%';
+    this.canvas.nativeElement.style.backgroundPosition = '0px 0px';
+    this.canvas.nativeElement.style.backgroundImage = '';
+    this.isZoom = false;
+    this.canvas.nativeElement.parentElement.classList.add('dec-sysfile-bg-load');
+    const downloadImage = new Image();
+
+    downloadImage.onload = () => {
+      this.canvas.nativeElement.style.backgroundImage = 'url(' + downloadImage.src + ' )';
+      this.canvas.nativeElement.style.backgroundRepeat = 'no-repeat';
+      this.canvas.nativeElement.style.backgroundSize = 'auto ' + this.canvasSize + 'px';
+      this.canvas.nativeElement.parentElement.classList.remove('dec-sysfile-bg-load');
+      this.imgExternalLink = downloadImage.src;
+      this.imageSizeRaw = {
+        width: downloadImage.width,
+        height: downloadImage.height
+      }
+    };
+
+if (this.renders.length < 1) {
+    return;
+  }
+  downloadImage.src = this.getImageUrl(index);
+}
+  if (this.isZoom && mousedown) {
+        const coords = getPos(e),
+          x = coords[0],
+          y = coords[1];
+
+        if (((x - this.deltaX) * (-1)) + el.width < this.imageSize) {
+          this.offsetX = x - this.deltaX;
+        }
+
+        if (((y - this.deltaY) * (-1)) + el.width < this.imageSize) {
+          this.offsetY = y - this.deltaY;
+        }
+
+        if (this.offsetY > 0) {
+          this.offsetY = 0;
+        }
+        if (this.offsetX > 0) {
+          this.offsetX = 0;
+        }
+
+        el.style.backgroundPosition = this.offsetX + 'px ' + this.offsetY + 'px';
+
+        return;
+      }
+      
+function getPos(e) {
+    const r = el.getBoundingClientRect(),
+    x = e.clientX - r.left,
+    y = e.clientY - r.top;
+    return [x, y];
+}
